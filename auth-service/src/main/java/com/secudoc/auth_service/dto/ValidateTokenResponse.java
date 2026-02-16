1a87ac83b8b33b77622edb5265ebd115628ac78d
CHINMAY240799 <86231273+CHINMAY240799@users.noreply.github.com>
2026-02-16 21:40:57 +0530

    Validate API Created

---
+++ auth-service/src/main/java/com/secudoc/auth_service/dto/ValidateTokenResponse.java
@@ -0,0 +1,32 @@
+package com.secudoc.auth_service.dto;
+
+import java.util.List;
+
+public class ValidateTokenResponse {
+
+    private boolean valid;
+    private String username;
+    private List<String> roles;
+
+    public ValidateTokenResponse(boolean valid) {
+        this.valid = valid;
+    }
+
+    public ValidateTokenResponse(boolean valid, String username, List<String> roles) {
+        this.valid = valid;
+        this.username = username;
+        this.roles = roles;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+}
--- auth-service/src/main/java/com/secudoc/auth_service/security/JwtService.java
+++ auth-service/src/main/java/com/secudoc/auth_service/security/JwtService.java
@@ -5,7 +5,9 @@ import java.time.temporal.ChronoUnit;
 
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
@@ -14,11 +16,14 @@ import org.springframework.stereotype.Service;
 public class JwtService {
 
     private final JwtEncoder jwtEncoder;
+    private final JwtDecoder jwtDecoder;
 
-    public JwtService(JwtEncoder jwtEncoder) {
+    public JwtService(JwtEncoder jwtEncoder, JwtDecoder jwtDecoder ) {
         this.jwtEncoder = jwtEncoder;
+        this.jwtDecoder = jwtDecoder;
     }
 
+    // This method returns a JWT. Header and Claims is build below and JWTEndocder already has the signature.
     public String generateToken(String username) {
 
         Instant now = Instant.now();
@@ -37,4 +42,27 @@ public class JwtService {
         return jwtEncoder.encode(JwtEncoderParameters.from(jwsHeader,claims))
                 .getTokenValue();
     }
+    
+    // ---------- VALIDATE TOKEN ----------
+    public Jwt validateToken(String token) {
+        try {
+            return jwtDecoder.decode(token); // verifies signature + expiry automatically
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public boolean isTokenValid(String token) {
+        return validateToken(token) != null;
+    }
+
+    public String extractUsername(String token) {
+        Jwt jwt = validateToken(token);
+        return jwt != null ? jwt.getSubject() : null;
+    }
+
 }
+
+
+
+
